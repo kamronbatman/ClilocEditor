9e2d366856aeaa42c1d330456fd52436b6df4d9e
Kamron Batman <batman@sipprs.com>
2016-11-14 23:56:21 -0800

    [Update] Updates to visual studio 2015 w/ .NET Framework 4.6.1, and optimizes loading.

---
+++ ClilocEditorForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace ClilocEditor
+{
+    partial class ClilocEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClilocEditorForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LoadFileButton = new System.Windows.Forms.Button();
+            this.ProjectPath = new System.Windows.Forms.TextBox();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.statusBar1 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FindFileButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveFileButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.statusBar1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.LoadFileButton);
+            this.panel1.Controls.Add(this.ProjectPath);
+            this.panel1.Controls.Add(this.menuStrip2);
+            this.panel1.Location = new System.Drawing.Point(0, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(533, 85);
+            this.panel1.TabIndex = 2;
+            // 
+            // LoadFileButton
+            // 
+            this.LoadFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadFileButton.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.LoadFileButton.Location = new System.Drawing.Point(12, 32);
+            this.LoadFileButton.Name = "LoadFileButton";
+            this.LoadFileButton.Size = new System.Drawing.Size(509, 47);
+            this.LoadFileButton.TabIndex = 2;
+            this.LoadFileButton.Text = "Load The Cliloc File Entries Below...";
+            this.LoadFileButton.UseVisualStyleBackColor = true;
+            this.LoadFileButton.Click += new System.EventHandler(this.LoadFileButton_Click);
+            // 
+            // ProjectPath
+            // 
+            this.ProjectPath.Location = new System.Drawing.Point(12, 3);
+            this.ProjectPath.Name = "ProjectPath";
+            this.ProjectPath.Size = new System.Drawing.Size(509, 20);
+            this.ProjectPath.TabIndex = 2;
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(533, 24);
+            this.menuStrip2.TabIndex = 4;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel1});
+            this.statusBar1.Location = new System.Drawing.Point(0, 397);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(533, 22);
+            this.statusBar1.TabIndex = 1;
+            this.statusBar1.Text = "statusBar1";
+            // 
+            // StatusLabel1
+            // 
+            this.StatusLabel1.Margin = new System.Windows.Forms.Padding(13, 3, 0, 2);
+            this.StatusLabel1.Name = "StatusLabel1";
+            this.StatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.StatusLabel1.Text = "Status";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(234, 403);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Operation Time Depends On The Speed Of Your Machine...";
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.LabelEdit = true;
+            this.listView1.LabelWrap = false;
+            this.listView1.Location = new System.Drawing.Point(12, 126);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(509, 261);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ASSIGNED TEXT STRINGS";
+            this.columnHeader1.Width = 416;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "CLILOC #";
+            this.columnHeader2.Width = 91;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FindFileButton,
+            this.SaveFileButton});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(533, 36);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FindFileButton
+            // 
+            this.FindFileButton.Image = ((System.Drawing.Image)(resources.GetObject("FindFileButton.Image")));
+            this.FindFileButton.Margin = new System.Windows.Forms.Padding(228, 4, 0, 0);
+            this.FindFileButton.Name = "FindFileButton";
+            this.FindFileButton.Size = new System.Drawing.Size(152, 28);
+            this.FindFileButton.Text = "Search For Cliloc File";
+            this.FindFileButton.Click += new System.EventHandler(this.FindFileButton_Click);
+            // 
+            // SaveFileButton
+            // 
+            this.SaveFileButton.Image = ((System.Drawing.Image)(resources.GetObject("SaveFileButton.Image")));
+            this.SaveFileButton.Margin = new System.Windows.Forms.Padding(0, 4, 0, 0);
+            this.SaveFileButton.Name = "SaveFileButton";
+            this.SaveFileButton.Size = new System.Drawing.Size(144, 28);
+            this.SaveFileButton.Text = "Save The Cliloc File";
+            this.SaveFileButton.Click += new System.EventHandler(this.SaveFileButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 31);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ClilocEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 419);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ClilocEditorForm";
+            this.Text = "Cliloc Editor v2.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ClilocEditorForm_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ClilocEditorForm_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusBar1.ResumeLayout(false);
+            this.statusBar1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button LoadFileButton;
+        private System.Windows.Forms.TextBox ProjectPath;
+        private System.Windows.Forms.StatusStrip statusBar1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FindFileButton;
+        private System.Windows.Forms.ToolStripMenuItem SaveFileButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ClilocEditorForm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClilocEditor
+{
+    public partial class ClilocEditorForm : Form
+    {
+        private Thread Read;
+        private Stream t;
+        private Thread Write;
+        private BinaryReader r;
+        private BinaryWriter w;
+
+        public ClilocEditorForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Form Button Selections
+
+        private void LoadFileButton_Click(object sender, EventArgs e)
+        {
+            this.Read = new Thread(new ThreadStart(this.LoadFile));
+            this.Read.Start();
+            while (!this.Read.IsAlive){}
+        }
+
+        private void SaveFileButton_Click(object sender, EventArgs e)
+        {
+            this.Write = new Thread(new ThreadStart(this.SaveFile));
+            this.Write.Start();
+            while (!this.Write.IsAlive){}
+        }
+
+        private void FindFileButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog()
+            {
+                SelectedPath = this.ProjectPath.Text
+            };
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.ProjectPath.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        #endregion
+
+        #region Load And Save Routines
+
+        public void LoadFile()
+        {
+            unsafe
+            {
+                FileInfo fileInfo = new FileInfo(string.Concat(this.ProjectPath.Text, "\\cliloc.enu"));
+                try
+                {
+                    this.t = new FileStream(fileInfo.FullName, FileMode.Open);
+                }
+                catch
+                {
+                    this.StatusLabel1.Text = "ERROR: Bad Directory!";
+                    return;
+                }
+                this.StatusLabel1.Text = "Loading...";
+                this.r = new BinaryReader(this.t, Encoding.UTF8);
+                this.r.BaseStream.Seek((long)6, SeekOrigin.Begin);
+                int num = 0;
+
+                List<ListViewItem> items = new List<ListViewItem>();
+
+                while (this.r.BaseStream.Length != this.r.BaseStream.Position)
+                {
+                    uint num1 = this.r.ReadUInt32();
+                    this.r.ReadByte();
+                    ListViewItem item = new ListViewItem(Encoding.UTF8.GetString(this.r.ReadBytes((int)this.r.ReadUInt16())));
+                    item.SubItems.Add(num1.ToString());
+
+                    items.Add(item);
+                    num++;
+                }
+                this.t.Close();
+                this.r.Close();
+
+                this.listView1.Items.AddRange(items.ToArray());
+
+                this.StatusLabel1.Text = string.Concat("Load completed, ", num, " records");
+            }
+        }
+
+        public void SaveFile()
+        {
+            FileInfo fileInfo = new FileInfo(string.Concat(this.ProjectPath.Text, "\\cliloc.enu"));
+            try
+            {
+                this.t = new FileStream(fileInfo.FullName, FileMode.Create);
+            }
+            catch
+            {
+                this.StatusLabel1.Text = "ERROR: Bad Directory!";
+                return;
+            }
+            this.StatusLabel1.Text = "Saving...";
+            this.w = new BinaryWriter(this.t);
+            byte[] numArray = new byte[] { 2, 0, 0, 0, 1, 0 };
+            this.w.Write(numArray);
+            for (int i = 0; i < this.listView1.Items.Count; i++)
+            {
+                this.w.Write(Convert.ToUInt32(this.listView1.Items[i].SubItems[1].Text));
+                this.w.Write((byte)0);
+                byte[] bytes = Encoding.UTF8.GetBytes(this.listView1.Items[i].Text);
+                int length = bytes.GetLength(0);
+                this.w.Write(Convert.ToUInt16(length));
+                this.w.Write(bytes);
+            }
+            this.t.Close();
+            this.w.Close();
+            this.StatusLabel1.Text = "Save completed";
+        }
+
+        #endregion
+
+        private void ClilocEditorForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.t != null)
+            {
+                this.t.Close();
+            }
+            if (this.r != null)
+            {
+                this.r.Close();
+            }
+            if (this.Read != null)
+            {
+                this.Read.Abort();
+            }
+        }
+
+        private void ClilocEditorForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
